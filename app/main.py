d9744baf99cb9131df70e48798360e0a0a9def5d
pavel <pavel@DESKTOP-33FO31S>
2025-05-03 16:39:45 +0300

    New Init

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello from Demo App!!!"}
+    return {"message": "Hello, World!"}
 
 @app.get("/health")
 def health_check():
